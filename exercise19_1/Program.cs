8f6d4d5f21a6fda32fea92c92838940bc2cf5f08
badaev52 <badaev52@gmail.com>
2022-12-04 21:58:50 +0300

    end exercise 19_1

--- exercise19_1/Program.cs
+++ exercise19_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите координаты точки A: ");
+int Ax = inputInt("Ax");
+int Ay = inputInt("Ay");
+int Az = inputInt("Az");
+Console.WriteLine("Введите координаты точки B: ");
+int Bx = inputInt("Bx");
+int By = inputInt("By");
+int Bz = inputInt("Bz");
+
+Console.WriteLine("Расстояние между точками равно: " + distanceBetweenPoints(Ax, Ay, Az, Bx, By, Bz));
+
+int inputInt(string name)
+{
+    Console.WriteLine("Введите число " + name + ": ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+}
+
+double distanceBetweenPoints(int Ax, int Ay, int Az, int Bx, int By, int Bz){
+    double res;
+
+    res = Math.Pow((Math.Pow((Bx - Ax), 2) + Math.Pow((By - Ay), 2) + Math.Pow((Bz - Az), 2)), 0.5);
+
+    return res;
+
+}
