d93dcdfbedfc128f3525d25dd651917f43a2fb14
badaev52 <badaev52@gmail.com>
2022-12-04 22:38:26 +0300

    cod end

---
+++ exercise23/Program.cs
@@ -0,0 +1,43 @@
+﻿int num = inputInt();
+int[] array = new int[num];
+
+getArrayCubes(array, num);
+
+printArrayConsole(array);
+
+
+int inputInt()
+{
+    Console.WriteLine("Введите число: ");
+    bool isInt = int.TryParse(Console.ReadLine(), out int num);
+    if (isInt)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        return -1;
+    }
+}
+
+void getArrayCubes(int[] arr, int num)
+{
+
+    for (int i = 0; i < num; i++)
+    {
+
+        arr[i] = (int)Math.Pow(i+1, 3);
+
+    }
+
+}
+
+void printArrayConsole(int[] arr){
+
+    for(int i= 0; i< arr.Length; i++){
+        Console.Write(arr[i]);
+        if(i < arr.Length - 1) Console.Write(", ");
+    }
+
+}
